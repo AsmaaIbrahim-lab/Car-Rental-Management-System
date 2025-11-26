6ab60b5769cc128aa5ecac295dfa7c8ed5699766
AsmaaIbrahim-lab <asmaaabdelbakyy@gmail.com>
2025-11-26 21:13:35 +0200

    alter database schema

--- Car_rental_system/Car_rental_system/obj/Debug/net8.0/Car_rental_system.AssemblyInfo.cs
+++ Car_rental_system/Car_rental_system/obj/Debug/net8.0/Car_rental_system.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Car_rental_system")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b7ce0ca094435144b06c4434e9fd263d7f67d37")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b348a7b7fbebae8f932657765b8ef8f86946ed1a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Car_rental_system")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Car_rental_system")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
