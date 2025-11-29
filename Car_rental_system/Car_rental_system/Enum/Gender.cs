41e7b0b095cea2d62fb92b135512693241727713
AsmaaIbrahim-lab <asmaaabdelbakyy@gmail.com>
2025-11-29 15:15:57 +0200

    remove old project structre

--- Car_rental_system/Car_rental_system/Enum/Gender.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Car_rental_system.Enum
-{
-    public enum Gender
-    {
-        Male = 1,
-        Female = 2
-    }
-}
--- Car_rental_system/Car_rental_system/Enum/Role.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Car_rental_system.Enum
-{
-    public enum Role
-    {
-        Customer=1,CarOwner=2,Admin=3
-
-    }
-}
